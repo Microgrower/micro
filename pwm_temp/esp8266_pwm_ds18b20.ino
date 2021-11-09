dd5c0cd0c7f6f948684c4af53a5119a9b21428e4
Microgrower <91039258+Microgrower@users.noreply.github.com>
2021-11-09 02:31:18 +0000

    Update esp8266_pwm_ds18b20.ino

--- pwm_temp/esp8266_pwm_ds18b20.ino
+++ pwm_temp/esp8266_pwm_ds18b20.ino
@@ -1,32 +1,35 @@
 #include <OneWire.h>
-#include <DallasTemperature.h> 
+#include <DallasTemperature.h>
 #define ONE_WIRE_BUS D4
 int fanPin = 14;
 int dutyCycle = 0;
+unsigned long timing;
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 void setup() {
   Serial.begin(115200);
-  pinMode(fanPin, OUTPUT); 
-  sensors.begin(); 
+  pinMode(fanPin, OUTPUT);
+  sensors.begin();
   analogWriteRange(100);
   analogWriteFreq(15000);
 }
 float readSensorTemp() {
   sensors.requestTemperatures();
-  Serial.print("Temperature: ");  
+  Serial.print("Temperature: ");
   Serial.println(sensors.getTempCByIndex(0));
   return sensors.getTempCByIndex(0);
 }
 void controlFanSpeed (int fanSpeedPercent) {
   Serial.print("Fan Speed: ");
   Serial.print(fanSpeedPercent);
-  Serial.println("%");  
-  analogWrite(fanPin, fanSpeedPercent); 
+  Serial.println("%");
+  analogWrite(fanPin, fanSpeedPercent);
 }
 void loop() {
-  float sensorTemp = readSensorTemp();
-  int fanSpeedPercent = map(sensorTemp, 23, 36, 30, 100);
-  controlFanSpeed (fanSpeedPercent); 
-  delay(1000);
+  if (millis() - timing > 10000) {
+    timing = millis();
+    float sensorTemp = readSensorTemp();
+    int fanSpeedPercent = map(sensorTemp, 23, 36, 30, 100);
+    controlFanSpeed (fanSpeedPercent);
+  }
 }
